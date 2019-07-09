305751852282b836c870a266402a668336566911
KirillDeshkev <kirill.deshkev@gmail.com>
2019-07-09 23:07:33 +0300

    Mark tests for code review

--- test_product_page.py
+++ test_product_page.py
@@ -15,6 +15,7 @@ class TestUserAddToCartFromProductPage(object):
         login_page.register_new_user(login_page.get_email(), login_page.get_password())
         login_page.should_be_authorized_user()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_cart(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
         page = ProductPage(browser, link)
@@ -32,6 +33,7 @@ class TestUserAddToCartFromProductPage(object):
         page.should_not_be_success_message()
 
 
+@pytest.mark.need_review
 def test_guest_can_add_product_to_cart(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser, link)
@@ -57,6 +59,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -66,6 +69,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     login_page.should_be_login_page()
 
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_cart_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
