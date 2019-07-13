378488b9b8eb78a10bab8be9cbd6023309fb2145
KirillDeshkev <kirill.deshkev@gmail.com>
2019-07-13 15:52:58 +0300

    Move links to the top of the files

--- test_product_page.py
+++ test_product_page.py
@@ -4,12 +4,15 @@ from pages.product_page import ProductPage
 from pages.login_page import LoginPage
 from pages.cart_page import CartPage
 
+login_page_link = "http://selenium1py.pythonanywhere.com/accounts/login/"
+promo_page_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+product_page_link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+
 
 class TestUserAddToCartFromProductPage(object):
     @pytest.fixture(scope="function", autouse=True)
     def setup(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/accounts/login/"
-        login_page = LoginPage(browser, link)
+        login_page = LoginPage(browser, login_page_link)
         login_page.open()
         login_page.should_be_login_page()
         login_page.register_new_user(login_page.get_email(), login_page.get_password())
@@ -17,8 +20,7 @@ class TestUserAddToCartFromProductPage(object):
 
     @pytest.mark.need_review
     def test_user_can_add_product_to_cart(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-        page = ProductPage(browser, link)
+        page = ProductPage(browser, promo_page_link)
         page.open()
         product_name = page.get_product_name()
         product_price = page.get_product_price()
@@ -27,16 +29,14 @@ class TestUserAddToCartFromProductPage(object):
         page.should_be_added_to_cart(product_name, product_price)
 
     def test_user_cant_see_success_message(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-        page = ProductPage(browser, link)
+        page = ProductPage(browser, promo_page_link)
         page.open()
         page.should_not_be_success_message()
 
 
 @pytest.mark.need_review
 def test_guest_can_add_product_to_cart(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-    page = ProductPage(browser, link)
+    page = ProductPage(browser, promo_page_link)
     page.open()
     product_name = page.get_product_name()
     product_price = page.get_product_price()
@@ -46,23 +46,20 @@ def test_guest_can_add_product_to_cart(browser):
 
 
 def test_guest_cant_see_success_message(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-    page = ProductPage(browser, link)
+    page = ProductPage(browser, promo_page_link)
     page.open()
     page.should_not_be_success_message()
 
 
 def test_guest_should_see_login_link_on_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
+    page = ProductPage(browser, product_page_link)
     page.open()
     page.should_be_login_link()
 
 
 @pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
+    page = ProductPage(browser, product_page_link)
     page.open()
     page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
@@ -71,8 +68,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
 
 @pytest.mark.need_review
 def test_guest_cant_see_product_in_cart_opened_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
+    page = ProductPage(browser, product_page_link)
     page.open()
     page.go_to_cart_page()
     cart_page = CartPage(browser, browser.current_url)
